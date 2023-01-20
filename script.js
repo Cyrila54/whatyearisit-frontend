c5c54834ae8e09be2b970c5fa94538327f13ba77
Cyrila54 <cyril.a54@icloud.com>
2023-01-20 13:06:01 +0100

    message

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ fetch ('whatyearisit-backend-lilac.vercel.app/year')
 .then ((response)=>response.json())
 .then ((data)=>{
         console.log(data);
-    document.querySelector('#year').textContent= "vv"; //data.year
+    document.querySelector('#year').textContent= "vv" //data.year
 
 })
